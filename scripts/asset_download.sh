1a98a014a8bd036025bd5363a49917ead28e815b
Gabriel Valencia <gvalenc@us.ibm.com>
2020-02-11 14:36:58 -0600

    debug asset downloader

--- scripts/asset_download.sh
+++ scripts/asset_download.sh
@@ -19,6 +19,7 @@ echo "Fetching deployment assets for skit $SKIT_NAME"
 # directory structure: REPO/deployment-assets/<skit-name>/<deploy-target>
 # can't seem to find an easy way to d/l a specific folder, so need to get the whole repo
 curl $DEVX_GIT_URL_CODE/tar.gz/master | tar -xz
+ls -al
 if [ "$DEPLOY_TARGET" == "helm" ]; then mv devx-skit-governance-master/deployment-assets/$SKIT_NAME/$DEPLOY_TARGET ./chart; fi
 if [ "$DEPLOY_TARGET" == "knative" ]; then mv devx-skit-governance-master/deployment-assets/$SKIT_NAME/$DEPLOY_TARGET ./; fi
 if [ "$DEPLOY_TARGET" == "cf" ]; then mv devx-skit-governance-master/deployment-assets/$SKIT_NAME/$DEPLOY_TARGET ./; fi
