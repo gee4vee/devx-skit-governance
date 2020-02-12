63b6d1eee11d64965988f29c72c50857e00522d6
Gabriel Valencia <gvalenc@us.ibm.com>
2020-02-11 18:40:45 -0600

    debug asset downloader

--- scripts/asset_download.sh
+++ scripts/asset_download.sh
@@ -13,15 +13,16 @@ ls -al
 export SKIT_NAME=(${GIT_URL##*/})
 export SKIT_NAME=(${SKIT_NAME%%.*})
 
-echo "Fetching deployment assets for skit $SKIT_NAME"
+echo "Fetching deployment assets for skit ${SKIT_NAME}"
 # directory structure: REPO/deployment-assets/<skit-name>/<deploy-target>
 # can't seem to find an easy way to d/l a specific folder, so need to get the whole repo
 curl $DEVX_GIT_URL_CODE/tar.gz/master | tar -xz
 ls -al
-ls -al devx-skit-governance-master
-if [ "$DEPLOY_TARGET" == "helm" ]; then mv devx-skit-governance-master/deployment-assets/${SKIT_NAME}/${DEPLOY_TARGET} ./chart; fi
-if [ "$DEPLOY_TARGET" == "knative" ]; then mv devx-skit-governance-master/deployment-assets/${SKIT_NAME}/${DEPLOY_TARGET}/service.yaml ./; fi
-if [ "$DEPLOY_TARGET" == "cf" ]; then mv devx-skit-governance-master/deployment-assets/${SKIT_NAME}/${DEPLOY_TARGET}/manifest.yaml ./; fi
+ls -al devx-skit-governance-master/deployment-assets/${SKIT_NAME}/${DEPLOY_TARGET}
+
+if [ "${DEPLOY_TARGET}" == "helm" ]; then mv devx-skit-governance-master/deployment-assets/${SKIT_NAME}/${DEPLOY_TARGET} ./chart; fi
+if [ "${DEPLOY_TARGET}" == "knative" ]; then mv devx-skit-governance-master/deployment-assets/${SKIT_NAME}/${DEPLOY_TARGET}/service.yaml ./; fi
+if [ "${DEPLOY_TARGET}" == "cf" ]; then mv devx-skit-governance-master/deployment-assets/${SKIT_NAME}/${DEPLOY_TARGET}/manifest.yaml ./; fi
 
 rm -r devx-skit-governance-master
 ls -al
